ca1fcc6173a22cd7ff8d268714da8d70637a176a
AydinSalar <86121386+AydinSalar@users.noreply.github.com>
2024-10-22 22:54:08 -0400

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,5 +1,7 @@
-from app import app
-from models import db
+from app import app  # Import the Flask app instance
+from models import db  # Import the database instance from models
 
+# Use the Flask application context to ensure database actions are tied to the app
 with app.app_context():
+    # Create all database tables based on models defined in the application
     db.create_all()
